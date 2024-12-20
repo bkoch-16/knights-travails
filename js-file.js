ac581ec7487f141c93ec495054a079f2a355aab7
Ben Koch <178689076+bkoch-16@users.noreply.github.com>
2024-12-20 13:28:23 -0800

    created working setup with multiple helper fn
    created setup with helper functions where i store the child as a key and the parent as a value, to be able to backtrack

--- js-file.js
+++ js-file.js
@@ -22,50 +22,109 @@ function knightMoves(start, end) {
 
     const traversalQueue = new Queue();
     traversalQueue.enqueue(start);
-    const adjacencyList = {}; //add distance paramtter
+    const adjacencyList = {};
+    const visitedTile = {}
+
     while (traversalQueue.size() > 0) {
+      console.log("queue: " + traversalQueue.printQueue())
       const currentNode = traversalQueue.dequeue();
-      console.log(currentNode)
+      if (hasBeenVisited(currentNode, visitedTile)) {
+        continue
+      }
+
+      let previousLocation = findPreviousLocation(currentNode)
+
+      console.log("active node: " + currentNode)
+      console.log("previous location: " + previousLocation)
+      if (!adjacencyList[currentNode]) {
+        adjacencyList[currentNode] = [previousLocation]
+      }
+      else {
+        adjacencyList[currentNode].push(potentialMove)
+      }
+
+      if (currentNode[0] === end[0] && currentNode[1] === end[1]) {
+        console.log(adjacencyList);
+        console.log("Output: " + currentNode);
+        const outputArray = createFinalPath(adjacencyList, start, end)
+        console.log(`You made it in ${outputArray.length} moves! Here is your path:`)
+        for (let i = outputArray.length -1; i >= 0; i--) {
+          console.log(outputArray[i].toString())
+        }
+        return;
+      }
+     
+
       for (let i = 0; i < moveOptions.length; i++) {
+        previousLocation = findPreviousLocation(currentNode)
         const potentialMove = [
           currentNode[0] + moveOptions[i][0],
           currentNode[1] + moveOptions[i][1],
-          [currentNode]
+          previousLocation
         ];
-        if (
-          potentialMove[0] >= 0 &&
-          potentialMove[0] <= 7 &&
-          potentialMove[1] >= 0 &&
-          potentialMove[1] <= 7
-        ) {
-          traversalQueue.enqueue(potentialMove);
-          if (adjacencyList[currentNode]) {
-            let alreadyStored = false;
-            for (let i = 0; i < adjacencyList[currentNode].length; i++) {
-              if (
-                adjacencyList[currentNode][i][0] === potentialMove[0] &&
-                adjacencyList[currentNode][i][1] === potentialMove[1]
-              ) {
-                alreadyStored = true;
-                break;
-              }
-            }
-            if (alreadyStored === false) {
-              adjacencyList[currentNode].push(potentialMove);
-            }
-          } else {
-            adjacencyList[currentNode] = [potentialMove];
-          }
 
+        if (isInBounds(potentialMove) && !hasBeenVisited(currentNode, visitedTile)) {
+          console.log("QUEUE: " + potentialMove)
+          traversalQueue.enqueue(potentialMove);
         }
       }
-      if (currentNode[0] === end[0] && currentNode[1] === end[1]) {
-        console.log(adjacencyList);
-        console.log("Output:");
-        return;
-      }
+      visitedTile[currentNode.slice(0,2)] = true
+      console.log("complete: " + currentNode)
+      console.log(adjacencyList)
+
     }
   }
+}
+
+function createFinalPath(adjacencyList, start, end) {
+  const finalPath = [end]
+  let activeKey = [...end]
+
+  while (adjacencyList[activeKey][0][0] !== start[0] && adjacencyList[activeKey][0][1] !== start[1]) {
+    activeKey = finalPath[finalPath.length - 1]
+    finalPath.push(adjacencyList[activeKey][0])
+  }
+
+  return finalPath
+}
+
+function findPreviousLocation(currentNode) {
+  let previousLocation
+  if (currentNode.length > 2) {
+    previousLocation = currentNode.pop()
+
+  } else {
+    previousLocation = [...currentNode]
+  }
+  return previousLocation
+}
+function hasBeenVisited(currentNode, visitedTile) {
+  return visitedTile[currentNode.slice(0,2)]
+}
+
+
+function isInBounds(position) {
+  if (
+    position[0] >= 0 &&
+    position[0] <= 7 &&
+    position[1] >= 0 &&
+    position[1] <= 7
+  ) {
+    return true
+  }
+  else return false
+}
+
+function isAlreadyStored(adjacencyList, currentNode, potentialMove) {
+  for (let i = 0; i < adjacencyList[currentNode].length; i++) {
+    if (
+      adjacencyList[currentNode][i][0] === potentialMove[0] &&
+      adjacencyList[currentNode][i][1] === potentialMove[1]
+    ) {
+      return true
+    }
+  }
+  return false
 }
 
 class Queue {
